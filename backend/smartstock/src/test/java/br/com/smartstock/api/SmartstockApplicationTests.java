d9717530c2201c763d4199ec5e000cf3629e7083
marcelo0611 <mh0864622@gmail.com>
2026-03-20 09:30:18 -0300

    Commit inicial
    Criação de repositorios

---
+++ backend/smartstock/src/test/java/br/com/smartstock/api/SmartstockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartstock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
