70808f3642f3ef45fb0fbadcd35092869dfae3ea
marcelo0611 <mh0864622@gmail.com>
2026-03-20 10:37:08 -0300

    Commit entidade funcionario
    parte 1 da entidade funcionário

---
+++ backend/smartstock/src/main/java/br/com/smartstock/api/Funcionario.java
@@ -0,0 +1,68 @@
+package br.com.smartstock.api;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_funcionario")
+public class Funcionario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome do funcionário é obrigatório")
+	private String nome;
+	
+	private String login;
+	
+	private String senha;
+	
+	
+	public Funcionario() {}
+	
+	public Funcionario (String nome, String login, String senha) {
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+}
